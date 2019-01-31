7b3306a9592f194fb4fb80cb40a245c58bee7cb1
Antoni Angga <antoniangga14@gmail.com>
2019-01-31 15:11:36 +0700

    sign up done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  get '/login', to: 'static_pages#login'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'users#login'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
